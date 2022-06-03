dfe9468985999c23d0346871e1c5f071181dcee3
mike6321 <mike6321@naver.com>
2022-06-03 12:15:01 +0900

    [junwoo.choi] feat : project init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # reactive-programming
+
 리엑티브 프로그래밍을 위한 repository 입니다.
---
+++ src/test/java/me/choi/reactiveprogramming/ReactiveProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.reactiveprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
