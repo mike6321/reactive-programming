cfa8e866d14c78665a03bceb3ef4ece37e0ca423
mike6321 <mike6321@naver.com>
2022-07-12 09:08:59 +0900

    [junwoo.choi] feat : 스프링 리액티브 프로그래밍 (3) - Reactive Streams - Schedulers - subscibeOn

---
+++ src/main/java/me/choi/reactiveprogramming/section03/SchedulerEx.java
@@ -0,0 +1,65 @@
+package me.choi.reactiveprogramming.section03;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class SchedulerEx {
+
+    public static void main(String[] args) {
+        Publisher<Integer> publisher = subscriber -> {
+            subscriber.onSubscribe(new Subscription() {
+                @Override
+                public void request(long n) {
+                    log.debug("request");
+                    subscriber.onNext(1);
+                    subscriber.onNext(2);
+                    subscriber.onNext(3);
+                    subscriber.onNext(4);
+                    subscriber.onNext(5);
+                    subscriber.onComplete();
+                }
+
+                @Override
+                public void cancel() {
+
+                }
+            });
+        };
+
+        Publisher<Integer> subOnPub = sub -> {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            executorService.execute(() -> publisher.subscribe(sub));
+        };
+
+        subOnPub.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.debug("onNext : {}", integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError : {}", t);
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        });
+        System.out.println("exit");
+    }
+
+}
