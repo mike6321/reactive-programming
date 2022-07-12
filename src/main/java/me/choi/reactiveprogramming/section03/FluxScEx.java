39061ceac8a6c97dbbd1405c44cdfadd1e6faa8f
mike6321 <mike6321@naver.com>
2022-07-12 11:45:34 +0900

    [junwoo.choi] feat : 스프링 리액티브 프로그래밍 (3) - Reactive Streams - Schedulers - Flux (interval)

--- src/main/java/me/choi/reactiveprogramming/section03/FluxScEx.java
+++ src/main/java/me/choi/reactiveprogramming/section03/FluxScEx.java
@@ -1,18 +1,21 @@
 package me.choi.reactiveprogramming.section03;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class FluxScEx {
 
-    public static void main(String[] args) {
-        Flux.range(1, 10)
-                .publishOn(Schedulers.newSingle("pub"))
-                .log()
-                .subscribeOn(Schedulers.newSingle("sub"))
-                .subscribe(System.out::println);
-
-        System.out.println("exit");
+    public static void main(String[] args) throws InterruptedException {
+        Flux.interval(Duration.ofMillis(500))
+                .take(10)
+                .subscribe(s -> {
+                    log.debug("onNext : {}", s);
+                });
+        TimeUnit.SECONDS.sleep(10);
     }
 
 }
