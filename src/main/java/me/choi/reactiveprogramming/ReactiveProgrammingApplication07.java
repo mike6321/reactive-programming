0d66b73e3a32cf5f77fc8cad4ff587c287572e59
mike6321 <mike6321@naver.com>
2022-07-18 14:52:56 +0900

    [junwoo.choi] feat : 스프링 리액티브 프로그래밍 (5) - Netty4ClientHttpRequestFactory at AsyncRestTemplate

--- src/main/java/me/choi/reactiveprogramming/ReactiveProgrammingApplication07.java
+++ src/main/java/me/choi/reactiveprogramming/ReactiveProgrammingApplication07.java
@@ -1,9 +1,11 @@
 package me.choi.reactiveprogramming;
 
+import io.netty.channel.nio.NioEventLoopGroup;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.Netty4ClientHttpRequestFactory;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,11 @@ public class ReactiveProgrammingApplication07 {
     public static class MyController {
 
         private static final String URL = "/rest";
-        private AsyncRestTemplate asyncRestTemplate = new AsyncRestTemplate();
+        private AsyncRestTemplate asyncRestTemplate = new AsyncRestTemplate(
+                new Netty4ClientHttpRequestFactory(
+                        new NioEventLoopGroup(1)
+                )
+        );
         /**
          * 비동기 작업을 위해 백그라운드에 요청당 스레드를 생성한다.
          * (request 100 : -> thread : 100)
