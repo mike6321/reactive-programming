65935686cc1eda6a6bc1d4df920c800f98638c79
mike6321 <mike6321@naver.com>
2022-07-18 14:19:10 +0900

    [junwoo.choi] feat : 스프링 리액티브 프로그래밍 (5) - RemoteService 적용 (2sec delay)

--- src/main/java/me/choi/reactiveprogramming/ReactiveProgrammingApplication07.java
+++ src/main/java/me/choi/reactiveprogramming/ReactiveProgrammingApplication07.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @SpringBootApplication
@@ -14,10 +15,14 @@ public class ReactiveProgrammingApplication07 {
     public static class MyController {
 
         private static final String URL = "/rest";
+        private RestTemplate restTemplate = new RestTemplate();
 
         @GetMapping(URL)
         public String rest(int idx) {
-            return "rest : " + idx;
+            String res = restTemplate.getForObject("http://localhost:8081//service?req={req}",
+                    String.class,
+                    "hello" + idx);
+            return res;
         }
 
     }
