e012070efc8a77814e248bcdbbc6fa904c4ba0aa
mike6321 <mike6321@naver.com>
2022-07-21 17:16:25 +0900

    [junwoo.choi] feat : 스프링 리액티브 프로그래밍 (7) - CompletableFuture (Multi Thread)

--- src/main/java/me/choi/reactiveprogramming/section07/CFuture.java
+++ src/main/java/me/choi/reactiveprogramming/section07/CFuture.java
@@ -2,33 +2,28 @@ package me.choi.reactiveprogramming.section07;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @Slf4j
 public class CFuture {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
         CompletableFuture
                 .supplyAsync(() -> {
                     log.info("supplyAsync");
-                    if (1 == 1) {
-                        throw new RuntimeException();
-                    }
                     return 1;
                 })
                 .thenCompose((s) -> {
                     log.info("thenApply : {}", s);
                     return CompletableFuture.completedFuture(s + 1);
                 })
-                .thenApply((s) -> {
-                    log.info("thenApply : {}", s);
+                .thenApplyAsync((s) -> {
+                    log.info("thenApplyAsync : {}", s);
                     return s * 3;
-                })
+                }, executorService)
                 .exceptionally(e -> -10)
-                .thenAccept(s -> log.info("thenAccept : {}", s));
+                .thenAcceptAsync(s -> log.info("thenAcceptAsync : {}", s), executorService);
         log.info("exit");
         ForkJoinPool.commonPool().shutdown();
         ForkJoinPool.commonPool().awaitTermination(10, TimeUnit.SECONDS);
